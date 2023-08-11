1f1fada0d70fa3adc9467fb0650dff36ba56ade5
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-08-11 14:29:47 +0100

    1.1 Profile - Added profile route

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Experience } from './components';
+import { getData } from "./utils/getData";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = ({ }) => {
+
+  const [experience, setExperience] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      await getData({ setExperience });
+      setIsLoading(false);
+    };
+    fetchArticles();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path='/profile' element={<Experience experience={experience} />} />
+      </Routes>
+    </Router>
   )
 }
-
-export default App
+export default App;
