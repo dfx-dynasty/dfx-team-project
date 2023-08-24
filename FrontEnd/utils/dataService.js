919b041254ca175e967e1eb18fe2d88203422071
Aimable Mardel <73969781+aimardel@users.noreply.github.com>
2023-08-24 11:38:21 +0100

    Changed axios call to render for build

--- FrontEnd/utils/dataService.js
+++ FrontEnd/utils/dataService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const getUserData = async () => {
   try {
-    // const responseData = await axios.get("https://dfx-dynasty.onrender.com/getdata");
-    const responseData = await axios.get("http://localhost:3001/getdata");
+    const responseData = await axios.get("https://dfx-dynasty.onrender.com/getdata");
+    // const responseData = await axios.get("http://localhost:3001/getdata");
     return responseData.data;
   } catch (error) {
     return error;
@@ -12,9 +12,9 @@ export const getUserData = async () => {
 
 export const putBioDataService = async Bio => {
   try {
-    // const updateBio = await axios.put("https://dfx-dynasty.onrender.com/updatebio", Bio);
-    console.log(Bio)
-    const updateBio = await axios.put("http://localhost:3001/updatebio", Bio);
+    const updateBio = await axios.put("https://dfx-dynasty.onrender.com/updatebio", Bio);
+    // console.log(Bio)
+    // const updateBio = await axios.put("http://localhost:3001/updatebio", Bio);
     return { Bio: updateBio.data };
   } catch (error) {
     return error;
